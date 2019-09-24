d846040a3a6445020bb83cd40db621a430e4531d
Jonathan Mohan <JonathanMohan.net@gmail.com>
2019-09-24 02:11:04 -0500

    Built Navbar and foundation

---
+++ src/Pages/PageB/PageB.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './PageB.scss';
+
+
+class PageB extends React.Component {
+  state = {
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div className='pageB w-100'>
+        <h2>WELCOME TO PAGE B</h2>
+        <div className='PageAWrap'>
+        </div>
+        <div className="row justify-content-center"></div>
+      </div>
+    );
+  }
+}
+
+export default PageB;
---
+++ src/Pages/PageD/PageD.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './PageD.scss';
+
+
+class PageD extends React.Component {
+  state = {
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div className='pageD w-100'>
+        <h2>WELCOME TO PAGE D</h2>
+        <div className='PageAWrap'>
+        </div>
+        <div className="row justify-content-center"></div>
+      </div>
+    );
+  }
+}
+
+export default PageD;
